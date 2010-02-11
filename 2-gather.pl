df4f31b50ca2c5557c5f1f60077633f254aecf7b
Przemysław Wesołek <jest@go.art.pl>
2010-02-11 13:21:51 +0100

    Renamed file.

--- 2-gather.pl
+++ 2-gather.pl
@@ -4,8 +4,20 @@ use lib 'lib';
 use Mojolicious::Lite;
 use TwoGather::Vote;
 use TwoGather::VoteSet;
+use KiokuDB;
+sub kioku_dir {
+	return KiokuDB->connect("dbi:SQLite:dbname=siatka.db");
+}
+
 
 get '/' => 'index';
+
+get '/show' => sub {
+	my $self = shift;
+	my $dir = kioku_dir;
+	
+	$self->render('show');
+};
 
 get '/:groovy' => sub {
 	my $self = shift;
@@ -13,16 +25,22 @@ get '/:groovy' => sub {
 	$self->render('test');
 };
 
-use Data::Dumper;
-print Dumper(do_magic());
-print "OK";
-#shagadelic;
+
+#use Data::Dumper;
+#print Dumper(do_magic());
+#print "OK";
+shagadelic;
+
 
 sub do_magic {
 	my $v1 = TwoGather::Vote->new(opt => 'maybe', min_people => 3);
 	my $v2 = TwoGather::Vote->new(opt => 'yes');
 	my $vs1 = TwoGather::VoteSet->new;
 	$vs1->votes([$v1, $v2]);
+	
+	my $dir = kioku_dir;
+	my $scope = $dir->new_scope;
+	$dir->store($vs1);
 	return $vs1->hope_for;
 }
 
